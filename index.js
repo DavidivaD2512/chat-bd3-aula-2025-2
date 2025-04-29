5e3ddd78ce98c2cb62a58c1778722339005c6251
David Ramos <80305539+DavidivaD2512@users.noreply.github.com>
2025-04-29 20:39:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
+const socketIo = require('socket.io'); 
 const app = express();
 const server = http.createServer(app);
+const io = socketIo(server);
 const ejs = require('ejs');
 const path = require("path");
 
@@ -14,6 +16,30 @@ app.engine('html', ejs.renderFile);
 
 app.use('/', (req, resp) => {
     resp.render('index.html');
+});
+
+/* ##### LÓGICA DO SOCKET.IO - ENVIO E PROPAGAÇÃO DE MENSAGENS ##### */
+
+// ARRAY QUE SIMULA O BANCO DE DADOS
+let messages = [];
+
+/* ESTRUTURA DE CONEXÃO DO SOCKET.IO */
+io.on('connection', socket=>{
+
+    // Teste de conexão
+    console.log('NOVO USUÁRIO CONECTADO: ' + socket.id);
+
+    // Recupera e mantém (exibe) as mensagens entre o front e o back
+    socket.emit('previousMessage', messages);
+
+    // Lógica de chat quando uma mensagem é enviada:
+    socket.on('sendMessage', data=>{
+
+        // Adiciona a mensagem no final do array de mensagens:
+        messages.push(data);
+
+        socket.broadcast.emit('recivedMessage', data);
+    });
 });
 
 server.listen(3000, ()=>{console.log("CHAT RODANDO EM - http://localhost:3000")});
