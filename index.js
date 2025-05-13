e7c59481ea1eb6f5c5e5dd39f1a738cff842a260
David Ramos <80305539+DavidivaD2512@users.noreply.github.com>
2025-05-13 20:22:25 -0300

    Integração com mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require("path");
 
@@ -18,10 +19,32 @@ app.use('/', (req, resp) => {
     resp.render('index.html');
 });
 
+function conectDB() {
+    let dbURL = "mongodb+srv://David_Ramos:david123@cluster0.tnhlw.mongodb.net/"
+
+    mongoose.connect(dbURL);
+
+    mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
+    mongoose.connection.once('open', function() {
+        console.log('ATLAS MONGODB CONECTADO COM SUCESSO!');
+    });
+}
+
+conectDB();
+
+let Message = mongoose.model('message', {usuario:String, data_hora:String, message:String});
+
 /* ##### LÓGICA DO SOCKET.IO - ENVIO E PROPAGAÇÃO DE MENSAGENS ##### */
 
 // ARRAY QUE SIMULA O BANCO DE DADOS
 let messages = [];
+
+Message.find({})
+    .then(docs=>{
+        messages = docs
+    }).catch(error=>{
+        console.log(error)
+    });
 
 /* ESTRUTURA DE CONEXÃO DO SOCKET.IO */
 io.on('connection', socket=>{
@@ -36,9 +59,17 @@ io.on('connection', socket=>{
     socket.on('sendMessage', data=>{
 
         // Adiciona a mensagem no final do array de mensagens:
-        messages.push(data);
+        //messages.push(data);
+        let message = new Message(data);
 
-        socket.broadcast.emit('recivedMessage', data);
+        message.save()
+            .then(
+                socket.broadcast.emit('recivedMessage', data)
+            ).catch(error=>{
+                console.log(error)
+            });
+
+        
 
         console.log("QTD MENSAGENS: " + messages.length);
     });
